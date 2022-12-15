bdfbd2669d250b57e49cf8d88ed68307f3e4b35e
lirockk <arthurdemurger26@gmail.com>
2022-12-15 13:17:42 +0100

    add subjects

---
+++ CPP-00/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 12:55:04 by ademurge          #+#    #+#             */
+/*   Updated: 2022/12/15 13:15:43 by ademurge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main (int ac, char **av)
+{
+	int	i;
+	(void)	av;
+	i = 0;
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+}
