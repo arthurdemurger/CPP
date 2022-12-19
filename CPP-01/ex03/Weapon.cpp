0b600a680f055f4271e467234c121e4f1cdabb43
lirockk <arthurdemurger26@gmail.com>
2022-12-19 17:15:31 +0100

    CPP01 - work on ex03

--- CPP-01/ex03/Weapon.cpp
+++ CPP-01/ex03/Weapon.cpp
@@ -6,7 +6,25 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 16:52:19 by ademurge          #+#    #+#             */
-/*   Updated: 2022/12/19 16:52:20 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/12/19 17:15:21 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+
+Weapon::Weapon(void) : _type("default")
+{
+	std::cout << "Weapon created." << std::endl;
+	return ;
+}
+
+Weapon::Weapon(std::string type) : _type(type)
+{
+	std::cout << "Weapon created." << std::endl;
+	return ;
+}
+
+Weapon::~Weapon(void)
+{
+	std::cout << "Weapon destroyed." << std::endl;
+}
--- CPP-01/ex03/Weapon.hpp
+++ CPP-01/ex03/Weapon.hpp
@@ -6,7 +6,22 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 16:52:11 by ademurge          #+#    #+#             */
-/*   Updated: 2022/12/19 16:52:12 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/12/19 17:11:41 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+class Weapon
+{
+public:
+	Weapon();
+	Weapon(std::string type);
+	~Weapon();
+
+	void	setType(std::string type);
+	std::string const	&getType(void);
+private:
+	std::string _type;
+
+};
