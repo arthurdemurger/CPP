ab7308d24c4bcd9421ed759e3682568d9672c0f3
lirockk <98538729+lirockk@users.noreply.github.com>
2023-03-01 13:40:24 +0100

    add CPP03 - ex00

--- CPP-03/ex00/ClapTrap.cpp
+++ CPP-03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 14:12:32 by ademurge          #+#    #+#             */
-/*   Updated: 2023/02/28 14:59:52 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/03/01 12:37:34 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 ClapTrap::ClapTrap(void) : _name("Default"), _hp(10), _energy(10), _damage(0)
 {
-	std::cout << "Default constructor called" << std::endl;
+	std::cout << "ClapTrap " << this->_name << " default constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name) : _name(name), _hp(10), _energy(10), _damage(0)
 {
-	std::cout << "Name constructor called" << std::endl;
+	std::cout << "ClapTrap " << this->_name << " constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap &c)
 {
-	std::cout << "Copy constructor called" << std::endl;
+	std::cout << "ClapTrap " << this->_name << " copy constructor called" << std::endl;
 	*this = c;
 }
 
 ClapTrap::~ClapTrap(void)
 {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << "ClapTrap " << this->_name << " destructor called" << std::endl;
 }
 
 ClapTrap&	ClapTrap::operator=(const ClapTrap& rhs)
@@ -46,9 +46,9 @@ ClapTrap&	ClapTrap::operator=(const ClapTrap& rhs)
 void	ClapTrap::attack(const std::string& target)
 {
 	if (!this->_hp)
-		std::cout << "ClapTrap " << this->_name << "is dead." << std::endl;
+		std::cout << "ClapTrap " << this->_name << " is dead and cannot attack." << std::endl;
 	else if (!this->_energy)
-		std::cout << "ClapTrap " << this->_name << "is out of energy." << std::endl;
+		std::cout << "ClapTrap " << this->_name << " is out of energy and cannot attack" << std::endl;
 	else
 	{
 		std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_damage << " points of damage!" << std::endl;
@@ -59,17 +59,20 @@ void	ClapTrap::attack(const std::string& target)
 void	ClapTrap::takeDamage(unsigned int amount)
 {
 	if (!this->_hp)
-		std::cout << "ClapTrap " << this->_name << "is already dead." << std::endl;
+		std::cout << "ClapTrap " << this->_name << " is already dead." << std::endl;
 	else
 	{
-		std::cout << "ClapTrap " << this->_name << "takes " << amount << " points of damages.";
+		std::cout << "ClapTrap " << this->_name << " takes " << amount << " points of damages.";
 		if (this->_hp <= amount)
 		{
 			std::cout << " " << this->_name << " is now dead.";
 			this->_hp = 0;
 		}
 		else
+		{
 			this->_hp -= amount;
+			std::cout << " " << this->_hp << " hitpoints left.";
+		}
 		std::cout << std::endl;
 	}
 }
@@ -77,9 +80,9 @@ void	ClapTrap::takeDamage(unsigned int amount)
 void	ClapTrap::beRepaired(unsigned int amount)
 {
 	if (!this->_hp)
-		std::cout << "ClapTrap " << this->_name << "is dead and cannot repairs itself." << std::endl;
+		std::cout << "ClapTrap " << this->_name << " is dead and cannot repairs itself." << std::endl;
 	else if (!this->_energy)
-		std::cout << "ClapTrap " << this->_name << "is out of energy and cannot repairs itself." << std::endl;
+		std::cout << "ClapTrap " << this->_name << " is out of energy and cannot repairs itself." << std::endl;
 	else
 	{
 		this->_hp += amount;
--- CPP-03/ex00/main.cpp
+++ CPP-03/ex00/main.cpp
@@ -6,7 +6,24 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 14:59:55 by ademurge          #+#    #+#             */
-/*   Updated: 2023/02/28 14:59:56 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/03/01 12:39:06 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	int	i;
+
+	ClapTrap clap("Clap");
+	i = -1;
+	while (++i < 5)
+		clap.attack("Trap");
+	clap.takeDamage(5);
+	while (++i <= 10)
+		clap.beRepaired(1);
+	clap.takeDamage(10);
+	clap.attack("Trap");
+	return (EXIT_SUCCESS);
+}
