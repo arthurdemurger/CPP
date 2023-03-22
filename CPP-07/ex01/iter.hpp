8fa5ab42198e1669595f50b39e90a0d86fef254d
lirockk <arthurdemurger26@gmail.com>
2023-03-22 18:48:46 +0100

    add CPP-07 ex01

--- CPP-07/ex01/iter.hpp
+++ CPP-07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 13:00:32 by ademurge          #+#    #+#             */
-/*   Updated: 2023/03/22 14:02:06 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/03/22 18:45:23 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,22 @@
 #include <iostream>
 
 template <typename T>
-void	display(T &ref)
-{
-	std::cout << ref << std::endl;
-}
-
-template <typename T>
-void	iter(T *array, int length, void (f)(T const &ref))
+void	iter(T *array, int length, void (* f)(T &ref))
 {
 	for (int i = 0; i < length; i++)
 		f(array[i]);
+}
+
+template <typename T>
+void	display(T &ref)
+{
+	std::cout << ref << " ";
+}
+
+template <typename T>
+void	increment(T &ref)
+{
+	ref++;
 }
 
 #endif
--- CPP-07/ex01/main.cpp
+++ CPP-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 13:00:35 by ademurge          #+#    #+#             */
-/*   Updated: 2023/03/22 14:03:49 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/03/22 18:48:04 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,38 @@
 
 int main (void)
 {
-	char	string[3] = {'a', 'b', 'c'};
-	int		array[5] = {1, 2, 3, 4, 5};
+	{
+		char	string[3] = {'a', 'b', 'c'};
+		int		array[5] = {1, 2, 3, 4, 5};
 
-	iter(string, 3, display(string));
-	iter(array, 5, display(array));
+		std::cout << "*********** DISPLAY ***********" << std::endl;
+		std::cout << "display an array of char : ";
+		iter(string, 3, display);
+
+		std::cout << std::endl;
+
+		std::cout << "display an array of int  : ";
+		iter(array, 5, display);
+		std::cout << std::endl << std::endl;
+	}
+	{
+		char	string[3] = {'a', 'b', 'c'};
+		int		array[5] = {1, 2, 3, 4, 5};
+
+		std::cout << "*********** INCREMENT ***********" << std::endl;
+		std::cout << "[before incrementation] array of char : ";
+		iter(string, 3, display);
+		std::cout << std::endl;
+		std::cout << "[after  incrementation] array of char : ";
+		iter(string, 3, increment);
+		iter(string, 3, display);
+		std::cout << std::endl;
+		std::cout << "[before incrementation] array of int : ";
+		iter(array, 5, display);
+		std::cout << std::endl;
+		std::cout << "[after  incrementation] array of int : ";
+		iter(array, 5, increment);
+		iter(array, 5, display);
+	}
 	return (0);
 }
