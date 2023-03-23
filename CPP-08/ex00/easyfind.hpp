51ec8bd2071da4c33b661b7af42e2d3f227a77e8
lirockk <arthurdemurger26@gmail.com>
2023-03-23 15:12:12 +0100

    add CPP-0-8 ex00

--- CPP-08/ex00/easyfind.hpp
+++ CPP-08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 11:15:34 by ademurge          #+#    #+#             */
-/*   Updated: 2023/03/23 11:20:30 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/03/23 15:07:58 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,21 @@
 
 # define EASYFIND_HPP
 
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <list>
+#include <array>
+
 template <typename T>
-int	easyfind(T stack, int nb)
+typename T::iterator	easyfind(T stack, int nb)
 {
-	
+	typename T::iterator	it;
+
+	it = std::find(stack.begin(), stack.end(), nb);
+	if (it == stack.end())
+		throw std::exception();
+	return (it);
 }
 
 #endif
---
+++ CPP-08/ex00/main.cpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/23 14:02:31 by ademurge          #+#    #+#             */
+/*   Updated: 2023/03/23 15:11:20 by ademurge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+int main (void)
+{
+	{
+		std::cout << "********** vector container **********" << std::endl;
+		std::vector<int> itInt;
+
+		for(size_t i = 0; i < 10; ++i)
+			itInt.push_back(i);
+		try
+		{
+			std::cout << "find an existing element      [5] : ";
+			std::cout << *easyfind(itInt, 5) << " ";
+			std::cout << std::endl;
+			std::cout << "find an not existing element [-5] : ";
+			std::cout << *easyfind(itInt, -5) << std::endl;
+		}
+		catch (const std::exception &e)
+		{
+			std::cout << "Nothing found." << std::endl << std::endl;
+		}
+	}
+	{
+		std::cout << "********** list container **********" << std::endl;
+		std::list<int> itInt;
+
+		for(size_t i = 0; i < 10; ++i)
+			itInt.push_back(i);
+		try
+		{
+			std::cout << "find an existing element      [5] : ";
+			std::cout << *easyfind(itInt, 5) << " ";
+			std::cout << std::endl;
+			std::cout << "find an not existing element [-5] : ";
+			std::cout << *easyfind(itInt, -5) << std::endl;
+		}
+		catch (const std::exception &e)
+		{
+			std::cout << "Nothing found." << std::endl << std::endl;
+		}
+	}
+		{
+		std::cout << "********** array container **********" << std::endl;
+		std::array<int, 10> itInt;
+
+		for(size_t i = 0; i < 10; ++i)
+			itInt[i] = i;
+		try
+		{
+			std::cout << "find an existing element      [5] : ";
+			std::cout << *easyfind(itInt, 5) << " ";
+			std::cout << std::endl;
+			std::cout << "find an not existing element [-5] : ";
+			std::cout << *easyfind(itInt, -5) << std::endl << std::endl;
+		}
+		catch (const std::exception &e)
+		{
+			std::cout << "Nothing found." << std::endl;
+		}
+	}
+}
