8e9abe37a81e6ea4b699d9bff01ed5e638e2377b
lirockk <arthurdemurger26@gmail.com>
2023-03-23 10:34:37 +0100

    add CPP-07 ex02

---
+++ CPP-07/ex02/Array.hpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/23 10:00:43 by ademurge          #+#    #+#             */
+/*   Updated: 2023/03/23 10:26:14 by ademurge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+
+#define ARRAY_HPP
+
+#include <iostream>
+
+template <typename T>
+class Array
+{
+	public:
+		Array() : _size(0), _array(NULL) { }
+
+		Array(unsigned int n) : _size(n)
+		{
+			this->_array = new T[n];
+		}
+
+		Array(const Array &copy) : _size(copy._size)
+		{
+			this->_array = new T[copy._size];
+			for (unsigned int i = 0; i < copy._size; i++)
+				this->_array[i] = copy._array[i];
+		}
+
+		Array &operator=(const Array &copy)
+		{
+			if (this != &copy)
+			{
+				delete [] _array;
+				this->_size = copy._size;
+				this->_array = new T[this->_size];
+				for (int i = 0; i < copy._size; i++)
+					this->_array[i] = copy._array[i];
+			}
+			return (*this);
+		}
+
+		~Array()
+		{
+			if (this->_array)
+				delete [] this->_array;
+		}
+
+		T	&operator[](unsigned int index)
+		{
+			if (index < 0 || index > this->_size - 1)
+				throw std::out_of_range("error: index out of range.");
+			return (this->_array[index]);
+		}
+
+		unsigned int	size(void) { return (this->_size); }
+
+		private:
+			T				*_array;
+			unsigned int	_size;
+};
+
+
+
+#endif
---
+++ CPP-07/ex02/main.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/23 10:19:52 by ademurge          #+#    #+#             */
+/*   Updated: 2023/03/23 10:34:06 by ademurge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+int main(void)
+{
+	{
+		std::cout << "********** INT ARRAY **********" << std::endl;
+		Array<int> intArr(20);
+
+		std::cout << "----- Valid index -----" << std::endl;
+		try
+		{
+			intArr[10] = 20;
+			std::cout << intArr[10] << std::endl << std::endl;
+		}
+		catch (const std::out_of_range &oor)
+		{
+			std::cout << oor.what() << std::endl << std::endl;
+		}
+
+		std::cout << "----- Invalid index -----" << std::endl;
+		try
+		{
+			intArr[50] = 20;
+			std::cout << intArr[50] << std::endl << std::endl;
+		}
+		catch (const std::out_of_range &oor)
+		{
+			std::cout << oor.what() << std::endl << std::endl;
+		}
+
+		std::cout << "----- Size method -----" << std::endl;
+		std::cout << intArr.size() << std::endl << std::endl;
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "********** STRING ARRAY **********" << std::endl;
+		Array<std::string> stringArr(20);
+		std::string			str("This is a string.");
+
+		std::cout << "----- Valid index -----" << std::endl;
+		try
+		{
+			stringArr[10] = str;
+			std::cout << stringArr[10] << std::endl << std::endl;
+		}
+		catch (const std::out_of_range &oor)
+		{
+			std::cout << oor.what() << std::endl << std::endl;
+		}
+
+		std::cout << "----- Invalid index -----" << std::endl;
+		try
+		{
+			stringArr[50] = str;
+			std::cout << stringArr[50] << std::endl << std::endl;
+		}
+		catch (const std::out_of_range &oor)
+		{
+			std::cout << oor.what() << std::endl << std::endl;
+		}
+
+		std::cout << "----- Size method -----" << std::endl;
+		std::cout << stringArr.size() << std::endl << std::endl;
+	}
+}
