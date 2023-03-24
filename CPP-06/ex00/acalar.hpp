59811af0419559172a9c7d4d8fe467a82e069254
lirockk <arthurdemurger26@gmail.com>
2023-03-24 15:18:32 +0100

    tmp

--- CPP-06/ex00/acalar.hpp
+++ CPP-06/ex00/acalar.hpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 13:40:39 by ademurge          #+#    #+#             */
-/*   Updated: 2023/03/21 12:32:31 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/03/24 15:15:57 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,20 @@
 # define INT 2
 # define FLOAT 3
 # define DOUBLE 4
-# define ICI std::cout << "ici\n";
 
 class Scalar
 {
 	public:
+		/* Canonical form*/
 		~Scalar();
 		Scalar(std::string str);
 		Scalar(const Scalar &copy);
 		Scalar	&operator=(const Scalar &copy);
 
+		/* Accessor */
 		std::string	getStr(void) const;
 
+		/* Operators */
 		operator char();
 		operator int();
 		operator float();
