d576734f0e17c0fa5474f6b9ef47fa684e77c058
lirockk <arthurdemurger26@gmail.com>
2023-03-24 12:14:11 +0100

    add main structure

---
+++ CPP-09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/24 12:12:56 by ademurge          #+#    #+#             */
+/*   Updated: 2023/03/24 12:13:36 by ademurge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+
+#define BITCOINEXCHANGE_HPP
+
+class BitcoinExchange
+{
+
+};
+
+#endif
