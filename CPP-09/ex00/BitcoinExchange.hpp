3d73979967c17230c94b917e0fdcb1f46db4480f
lirockk <arthurdemurger26@gmail.com>
2023-03-27 11:38:46 +0200

    work on CPP-09 ex00

--- CPP-09/ex00/BitcoinExchange.hpp
+++ CPP-09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 12:12:56 by ademurge          #+#    #+#             */
-/*   Updated: 2023/03/24 12:13:36 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/03/27 11:38:29 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,20 @@
 
 #define BITCOINEXCHANGE_HPP
 
+#include <iostream>
+#include <map>
+
 class BitcoinExchange
 {
+	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange &copy);
+		BitcoinExchange &operator=(const BitcoinExchange &copy);
+		~BitcoinExchange();
 
+		std::map<int, float>	getMap(void) const;
+	private:
+		std::map<int, float>	_map;
 };
 
 #endif
--- CPP-09/ex00/main.cpp
+++ CPP-09/ex00/main.cpp
@@ -6,12 +6,19 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 12:11:25 by ademurge          #+#    #+#             */
-/*   Updated: 2023/03/24 12:11:56 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/03/27 11:33:00 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <map>
+
 int main (void)
 {
+	std::map <int, float> map;
+	map[212] = 3.04f;
+	map[21] = 1.0f;
 
+	std::cout << map.at(21) << std::endl;
 	return (0);
 }
