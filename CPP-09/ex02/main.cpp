92c254cdae08e4dce555fef33e1c5c2f3d478f13
lirockk <arthurdemurger26@gmail.com>
2023-03-30 13:39:25 +0200

    [CPP-09/ex02] : add minor change

--- CPP-09/ex02/main.cpp
+++ CPP-09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 12:11:25 by ademurge          #+#    #+#             */
-/*   Updated: 2023/03/30 13:34:33 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/03/30 13:38:37 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int main (int ac, char **av)
 	}
 	catch(const PmergeMe::WrongInputException& wie)
 	{
-		std::cerr << wie.what() << std::endl;
 		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
