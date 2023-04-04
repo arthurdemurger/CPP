3e38737cf8592a23dcaad5466805e8c444164234
lirockk <arthurdemurger26@gmail.com>
2023-04-04 11:27:23 +0200

    [CPP-07/ex00] : done

--- CPP-07/ex00/main.cpp
+++ CPP-07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 12:42:45 by ademurge          #+#    #+#             */
-/*   Updated: 2023/03/22 12:58:27 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/04/04 11:26:54 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,28 @@
 
 int main (void)
 {
+	srand(time(0));
 	{
 		std::cout << "********************** INT **********************" << std::endl;
-		int	a = -89;
-		int	b = -1;
+		int	a = rand() % 100;
+		int	b = rand() % 100;
 
 		std::cout << "------------ Before Swap ------------" << std::endl;
 		std::cout << "first  int : " << a << std::endl;
 		std::cout << "second int : " << b << std::endl;
-		std::cout << "max : " << ::max(a, b) << std::endl;
-		std::cout << "min : " << ::min(a, b) << std::endl;
+		std::cout << "max between [" << a << ", " << b << "] : " << ::max(a, b) << std::endl;
+		std::cout << "min between [" << a << ", " << b << "] : " << ::min(a, b) << std::endl;
 
 		::swap(a, b);
 
 		std::cout << "------------ After Swap ------------" << std::endl;
 		std::cout << "first  int : " << a << std::endl;
 		std::cout << "second int : " << b << std::endl;
-		std::cout << "max : " << ::max(a, b) << std::endl;
-		std::cout << "min : " << ::min(a, b) << std::endl;
+		std::cout << "max between [" << a << ", " << b << "] : " << ::max(a, b) << std::endl;
+		std::cout << "min between [" << a << ", " << b << "] : " << ::min(a, b) << std::endl;
 	}
+
+	std::cout << std::endl;
 
 	{
 		std::cout << "********************** STRING **********************" << std::endl;
@@ -42,15 +45,15 @@ int main (void)
 		std::cout << "------------ Before Swap ------------" << std::endl;
 		std::cout << "first  string : " << a << std::endl;
 		std::cout << "second string : " << b << std::endl;
-		std::cout << "max : " << ::max(a, b) << std::endl;
-		std::cout << "min : " << ::min(a, b) << std::endl;
+		std::cout << "max between [" << a << ", " << b << "] : " << ::max(a, b) << std::endl;
+		std::cout << "min between [" << a << ", " << b << "] : " << ::min(a, b) << std::endl;
 
 		::swap(a, b);
 
 		std::cout << "------------ After Swap ------------" << std::endl;
 		std::cout << "first  string : " << a << std::endl;
 		std::cout << "second string : " << b << std::endl;
-		std::cout << "max : " << ::max(a, b) << std::endl;
-		std::cout << "min : " << ::min(a, b) << std::endl;
+		std::cout << "max between [" << a << ", " << b << "] : " << ::max(a, b) << std::endl;
+		std::cout << "min between [" << a << ", " << b << "] : " << ::min(a, b) << std::endl;
 	}
 }
