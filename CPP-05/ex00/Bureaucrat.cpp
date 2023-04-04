dd101646da7a09bac022d3d713d875e62a71f706
lirockk <arthurdemurger26@gmail.com>
2023-04-04 09:53:28 +0200

    [CPP-05/ex00] : add changes for the correction

--- CPP-05/ex00/Bureaucrat.cpp
+++ CPP-05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 13:44:55 by ademurge          #+#    #+#             */
-/*   Updated: 2023/03/24 12:37:35 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/04/04 09:35:49 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ std::ostream& operator<<( std::ostream& o, const Bureaucrat& rhs ) {
 void	Bureaucrat::incrementGrade(void)
 {
 	if (this->_grade == 1)
-		throw Bureaucrat::GradeTooLowException();
+		throw Bureaucrat::GradeTooHighException();
 	else
 		this->_grade--;
 }
@@ -69,7 +69,7 @@ void	Bureaucrat::incrementGrade(void)
 void	Bureaucrat::decrementGrade(void)
 {
 	if (this->_grade == 150)
-		throw Bureaucrat::GradeTooHighException();
+		throw Bureaucrat::GradeTooLowException();
 	else
 		this->_grade++;
 }
