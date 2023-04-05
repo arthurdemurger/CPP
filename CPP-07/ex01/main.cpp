219a203c1d0e8c3201c73378b66397feb06a911d
lirockk <arthurdemurger26@gmail.com>
2023-04-05 10:38:42 +0200

    [CPP-07/ex01] : add minor changes

--- CPP-07/ex01/main.cpp
+++ CPP-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 13:00:35 by ademurge          #+#    #+#             */
-/*   Updated: 2023/04/04 11:40:58 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/04/05 10:38:03 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,22 @@
 
 int main (void)
 {
-	{
-		char	string[3] = {'a', 'b', 'c'};
-		int		array[5] = {1, 2, 3, 4, 5};
+	char	string[3] = {'a', 'b', 'c'};
+	int		array[5] = {1, 2, 3, 4, 5};
 
-		std::cout << "*********** DISPLAY ***********" << std::endl;
-		std::cout << "display an array of char : ";
-		iter(string, 3, display);
-
-		std::cout << std::endl;
-
-		std::cout << "display an array of int  : ";
-		iter(array, 5, display);
-		std::cout << std::endl << std::endl;
-	}
-	{
-		char	string[3] = {'a', 'b', 'c'};
-		int		array[5] = {1, 2, 3, 4, 5};
-
-		std::cout << "*********** INCREMENT ***********" << std::endl;
-		std::cout << "[before incrementation] array of char : ";
-		iter(string, 3, display);
-		std::cout << std::endl;
-		std::cout << "[after  incrementation] array of char : ";
-		iter(string, 3, increment);
-		iter(string, 3, display);
-		std::cout << std::endl;
-		std::cout << "[before incrementation] array of int : ";
-		iter(array, 5, display);
-		std::cout << std::endl;
-		std::cout << "[after  incrementation] array of int : ";
-		iter(array, 5, increment);
-		iter(array, 5, display);
-		std::cout << std::endl;
-	}
+	std::cout << "[before incrementation] array of char : ";
+	iter(string, 3, display);
+	std::cout << std::endl;
+	std::cout << "[after  incrementation] array of char : ";
+	iter(string, 3, increment);
+	iter(string, 3, display);
+	std::cout << std::endl;
+	std::cout << "[before incrementation] array of int  : ";
+	iter(array, 5, display);
+	std::cout << std::endl;
+	std::cout << "[after  incrementation] array of int  : ";
+	iter(array, 5, increment);
+	iter(array, 5, display);
+	std::cout << std::endl;
 	return (0);
 }
