41ebdcfdb7d7ff365db2aecd42eeee9586bc574e
lirockk <arthurdemurger26@gmail.com>
2023-04-05 09:34:44 +0200

    [CPP-09/ex01] : change variable name

--- CPP-09/ex01/main.cpp
+++ CPP-09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 12:11:25 by ademurge          #+#    #+#             */
-/*   Updated: 2023/03/28 15:25:04 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/04/05 09:32:15 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int main (int ac, char **av)
 {
-	RPN	calc;
+	RPN	reverse_polish_notation;
 
 	if (ac != 2)
 		return (RPN::put_error("Error: wrong number of arguments."));
-	calc.calculate(av[1]);
+	reverse_polish_notation.calculate(av[1]);
 	return (0);
 }
